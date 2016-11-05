efe1c10e5873071eb57a695e71ac52c40fc0bdc0
Ubuntu <remoteuser@ed0-azspar.fhyod4sono3ergw13qyn1vtmje.cx.internal.cloudapp.net>
2016-11-05 01:49:55 +0000

    update rstudio-server version

--- downloadRun.sh
+++ downloadRun.sh
@@ -90,7 +90,7 @@ sudo R --vanilla --quiet  <  /home/remoteuser/github_installs.R
 ## Install Rstudio
 ###########################################################################
 sudo cd /home/remoteuser
-# sudo wget http://mrsactionscripts.blob.core.windows.net/rstudio-server-community-v01/InstallRStudio.sh
+sudo wget https://raw.githubusercontent.com/akzaidi/mlads_spark_tutorial/master/InstallRStudio.sh
 sudo chmod 755 InstallRStudio.sh
 sudo ./InstallRStudio.sh
 
