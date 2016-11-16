01aca56658cd6ead2f24d4215fca4b2f7560e513
Ali-Kazim Zaidi <alikaz.zaidi@gmail.com>
2016-11-16 10:33:42 -0800

    add cluster verification scripts

---
+++ verification.sh
@@ -0,0 +1,12 @@
+mkdir /tmp/verif
+whoami > /tmp/verif/whoami
+pwd > /tmp/verif/pwd
+
+cd /home/remoteuser
+
+if [[ -f ClusterVerification.R ]]; then sudo rm -Rf ClusterVerification.R; fi;
+
+# UPDATE THE URL FOR MLADS
+wget https://raw.githubusercontent.com/akzaidi/mlads_spark_tutorial/master/ClusterVerification.R
+
+Rscript --default-packages= /home/remoteuser/ClusterVerification.R > /tmp/verif/rout.log
